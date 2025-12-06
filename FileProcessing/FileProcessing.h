8a057fea7397d1d01fbc3fd3d3cb66c87c30b337
StrelkovKM <strelkovkm96@outlook.com>
2025-12-06 15:10:51 +0300

    Edit FileProcessing.h

---
+++ FileProcessing/FileProcessing.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+
+class FileProcessing {
+    private:
+    std::vector<std::string> RAM;
+    size_t size_of_RAM_byte;
+    std::string name_of_file_output;
+    size_t len_of_file_input;
+    std::string name_of_file_input;
+    
+    //size_t size_of_RAM_Mbyte = size_of_RAM_byte / ( 1024 * 1024);
+
+    void SizeRAM(); //Егорка
+    void LenFile(std::string name_of_file); //Миша
+
+    public:
+    FileProcessing(std::string name_of_file_input_);
+
+    void CopyPartFileToRAM(size_t size_of_empty_RAM); 
+    void EraseFromFile(std::string name_of_file_input_, size_t size_to_erase); //Миша
+    void ShaffleInRAM(); //Егорка
+    void WriteToFile(std::string name_of_file_output_);
+
+};
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <random>
+#include <fstream>
+#include <chrono>
+
+// i+
+size_t random_len(size_t max_len) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<size_t> dis(1, max_len);
+    return dis(gen);
+}
+
+int main () {
+
+    std::ofstream inputFile("InputFile.txt");
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
+
+    if (inputFile.is_open()) {
+        size_t len_of_row = 1000;
+        for (size_t i = 0; i < len_of_row; i++) {
+            size_t len_of_str = random_len(1020);
+            inputFile << i;
+            for (size_t j = 0; j < len_of_str; j++) {
+                inputFile << 0;
+            }
+            inputFile << "\n";
+        }
+    }
+}
+
