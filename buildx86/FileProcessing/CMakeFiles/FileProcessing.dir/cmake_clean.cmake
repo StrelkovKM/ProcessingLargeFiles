463101c09becb490bb6389f8d39b71c245aba6a3
StrelkovKM <strelkovkm96@outlook.com>
2025-12-06 13:52:52 +0300

    Edit Directory && CMakeLists

---
+++ buildx86/FileProcessing/CMakeFiles/FileProcessing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileProcessing.dir/FileProcessing.cpp.o"
+  "CMakeFiles/FileProcessing.dir/FileProcessing.cpp.o.d"
+  "libFileProcessing.a"
+  "libFileProcessing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileProcessing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ buildx86/FileProcessing/CMakeFiles/FileProcessing.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FileProcessing.
+# This may be replaced when dependencies are built.
