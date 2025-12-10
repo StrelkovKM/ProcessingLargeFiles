c1d6c146f9079f38672ea216e69c684cab2d2768
Егор Турушкин <turushkine@inbox.ru>
2025-12-10 16:11:54 +0300

    Add windows version

--- FileProcessing/FileProcessing.cpp
+++ FileProcessing/FileProcessing.cpp
@@ -1,18 +1,46 @@
 #include "FileProcessing.h"
+#include "Windows.h"
+#include <algorithm>
 
 
 
 
 FileProcessing::FileProcessing(const std::string& name_of_file_input_,
     const std::string& name_of_file_output_)
-    : RAM(),
-    infile(name_of_file_input_),
-    outfile(name_of_file_output_)
+    : RAM()
 {
+    infile = CreateFile(name_of_file_input_.c_str(),
+        GENERIC_READ | GENERIC_WRITE | DELETE,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        FILE_FLAG_SEQUENTIAL_SCAN,
+        NULL
+    );
+
+    if (infile == INVALID_HANDLE_VALUE) {
+        throw std::runtime_error("Cannot open file. Error: " +
+            std::to_string(GetLastError()));
+    }
+
+    outfile = CreateFile(name_of_file_output_.c_str(),
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        FILE_FLAG_SEQUENTIAL_SCAN,
+        NULL
+    );
+
+    if (outfile == INVALID_HANDLE_VALUE) {
+        throw std::runtime_error("Cannot open file. Error: " +
+            std::to_string(GetLastError()));
+    }
+    
+    chunk = 0;
     this->SizeRAM();
     this->LenFile();
-    if (!infile.is_open() && !outfile.is_open())
-        throw "error open files";
+
 }
 
 void FileProcessing::SetSizeRAM(size_t size_of_ram)
@@ -20,28 +48,62 @@ void FileProcessing::SetSizeRAM(size_t size_of_ram)
     size_of_RAM_byte = size_of_ram;
 }
 
-/*
-void EraseFromFile(const std::string& name_of_file_input_, size_t size_to_erase) 
+void FileProcessing::CopyPartFileToRAM()
 {
-    HANDLE hFile = CreateFileA(name_of_file_input_.c_str(), GENERIC_WRITE, 0, NULL,
-                                OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE) {
-        throw std::runtime_error("Не удалось открыть файл для обрезки");
-    }
-    size_t new_size = len_of_file_input_ - size_to_erase;
+    size_t start_pos = (len_of_file_input - (size_of_RAM_byte - chunk) > 0)
+        ? (len_of_file_input - (size_of_RAM_byte - chunk)) : 0;
+    char symbol = '\0';
+
     LARGE_INTEGER li;
-    li.QuadPart = new_size;
+    li.QuadPart = static_cast<LONGLONG>(start_pos);
+    SetFilePointerEx(infile, li, NULL, FILE_BEGIN);
     
-    if (!SetFilePointerEx(hFile, li, NULL, FILE_BEGIN) || !SetEndOfFile(hFile)) {
-        CloseHandle(hFile);
-        throw std::runtime_error("Не удалось обрезать файл");
+    DWORD n;
+
+    while (symbol!=10)
+    {
+        ReadFile(infile, &symbol, 1, &n, NULL);
+        start_pos++;
     }
-    
-    CloseHandle(hFile);
+
+    chunk_erase = len_of_file_input - start_pos - 1;
+    std::cout << "[INFO] Chunk to erase: " << chunk_erase << " byte\n";
+
+    std::string buffer;
+    while (start_pos != len_of_file_input) {
+        ReadFile(infile, &symbol, 1, &n, NULL);
+        buffer.push_back(symbol);
+        if (symbol == 10)
+        {
+            RAM.push_back(buffer);
+            buffer.clear();
+        }
+        start_pos++;
+    }
+    std::cout << "[INFO] Copied to RAM: " << RAM.size() << " lines\n";
+    for (const auto& line : RAM) {
+        std::cout << line << "\n";
+    }
 }
-*/
 
+void FileProcessing::EraseFromFile()
+{
+    if (len_of_file_input == 0) {
+        std::cout << "[INFO] The file is already empty\n";
+        return;
+    }
 
+    size_t actual_erase = (((chunk_erase) < (len_of_file_input)) ? (chunk_erase) : (len_of_file_input));
+    LARGE_INTEGER li;
+    li.QuadPart = static_cast<LONGLONG>(actual_erase);
+    if (!SetFilePointerEx(infile, li , NULL, FILE_BEGIN))
+        throw("1");
+
+    // Обрезаем файл до этой позиции
+    if (!SetEndOfFile(infile))
+        throw("2");
+
+}
 
 void FileProcessing::SizeRAM()
 {
@@ -51,7 +113,7 @@ void FileProcessing::SizeRAM()
     if (GlobalMemoryStatusEx(&memInfo)) {
 
         std::cout << "Free RAM: " << memInfo.ullAvailPhys << " B\n";
-        size_of_RAM_byte = memInfo.ullAvailPhys;
+        size_of_RAM_byte = static_cast<size_t>(memInfo.ullAvailPhys*0.7);
     }
     else {
         std::cout << "Error of reading of RAM size\n";
@@ -63,10 +125,14 @@ void FileProcessing::SizeRAM()
 
 void FileProcessing::LenFile()
 {
-    std::streampos current_pos = infile.tellg();
-    infile.seekg(0, std::ios::end);
-    size_t size = infile.tellg();
-    infile.seekg(current_pos);
-    len_of_file_input = size;
-    std::cout << "File size: " << size << " B\n";
+    LARGE_INTEGER size_of_file;
+    if (!GetFileSizeEx(infile,&size_of_file)) {
+        CloseHandle(infile);
+        throw std::runtime_error("Cannot get file size");
+    }
+
+    if (size_of_file.QuadPart < 0) {
+        throw std::runtime_error("File size cannot be negative");
+    }
+    len_of_file_input = static_cast<size_t>(size_of_file.QuadPart);
 }
--- FileProcessing/FileProcessing.h
+++ FileProcessing/FileProcessing.h
@@ -10,8 +10,10 @@ class FileProcessing {
     std::vector<std::string> RAM;
     size_t size_of_RAM_byte;
     size_t len_of_file_input;
-    std::fstream infile;
-    std::fstream outfile;
+    HANDLE infile;
+    HANDLE outfile;
+    size_t chunk;
+    size_t chunk_erase;
     
     void SizeRAM(); //Егорка
     void LenFile(); //Миша
@@ -22,8 +24,8 @@ class FileProcessing {
     void SetSizeRAM(size_t size_of_ram);
 
 
-    void CopyPartFileToRAM(size_t size_of_empty_RAM); 
-    void EraseFromFile(std::string name_of_file_input_, size_t size_to_erase); //Миша
+    void CopyPartFileToRAM(); 
+    void EraseFromFile(); //Миша
     void ShaffleInRAM(); //Егорка
     void WriteToFile(std::string name_of_file_output_);
 };
