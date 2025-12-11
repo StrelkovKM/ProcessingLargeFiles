828f08c667caacf9006b44fbbe164ca5c008e8cf
StrelkovKM <strelkovkm96@outlook.com>
2025-12-11 20:59:53 +0300

    Edit Class

---
+++ FileProcessing/FileProcessing.cpp
@@ -0,0 +1,10 @@
+#include "FileProcessing.h"
+
+FileProcessing::FileProcessing(const std::string &filename) : 
+    memory_size(0), ram_size(0), start_write(0), start_read(0), chunk_read(0)
+{
+    file.open(filename, std::ios::in | std::ios::out | std::ios::binary);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+    }
+}
--- FileProcessing/FileProcessing.h
+++ FileProcessing/FileProcessing.h
@@ -5,7 +5,28 @@
 
 class FileProcessing {
     private:
+    std::fstream file;
+    std::vector<char> buffer;
+    std::vector<std::vector<char>> memory;
+    size_t memory_size;
+    size_t ram_size;
+    size_t start_write;
+    size_t start_read;
+    size_t chunk_read;
 
     public:
+    FileProcessing(const std::string& filename);
+    ~FileProcessing();
 
+    void sizeRAM();
+    void setRAMSize(size_t size);
+
+    void readFile(); //Egor
+    void spliteMemory(); //Misha
+    void clearSlice();
+    void shuffleMemory(); //Egor
+    void mergeSlice();
+    void writeFile(); //Misha
+
+    void executeProcessing();
 };
