500e4969e8b15866757a25ebed97019d10bf384f
StrelkovKM <strelkovkm96@outlook.com>
2025-12-11 16:29:43 +0300

    Delete All
    Reason: rewrite to crossplatform solution

--- FileProcessing/FileProcessing.h
+++ FileProcessing/FileProcessing.h
@@ -2,28 +2,13 @@
 #include <vector>
 #include <string>
 #include <fstream>
-#include <windows.h>
 
 
 class FileProcessing {
     private:
-    std::vector<std::string> RAM;
-    size_t size_of_RAM_byte;
-    size_t len_of_file_input;
-    std::fstream infile;
-    std::fstream outfile;
-    
-    void SizeRAM(); //Егорка
-    void LenFile(); //Миша
+
 
 
     public:
-    FileProcessing(const std::string& name_of_file_input_, const std::string& name_of_file_output_);
-    void SetSizeRAM(size_t size_of_ram);
 
-
-    void CopyPartFileToRAM(size_t size_of_empty_RAM); 
-    void EraseFromFile(std::string name_of_file_input_, size_t size_to_erase); //Миша
-    void ShaffleInRAM(); //Егорка
-    void WriteToFile(std::string name_of_file_output_);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,5 @@
-#include <iostream>
-#include <random>
-#include <fstream>
-#include <chrono>
-
 #include "FileProcessing.h"
 
-size_t random_len(size_t max_len) {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    std::uniform_int_distribution<size_t> dis(1, max_len);
-    return dis(gen);
+int main() {
+    return 0;
 }
-
-int main () {
-
-    //std::ofstream inputFile("InputFile.txt");
-    //if (!inputFile.is_open()) {
-    //    std::cerr << "Error opening file!" << std::endl;
-    //    return 1;
-    //}
-
-    //if (inputFile.is_open()) {
-    //    size_t len_of_row = 1000;
-    //    for (size_t i = 0; i < len_of_row; i++) {
-    //        size_t len_of_str = random_len(1020);
-    //        inputFile << i;
-    //        for (size_t j = 0; j < len_of_str; j++) {
-    //            inputFile << 0;
-    //        }
-    //        inputFile << "\n";
-    //    }
-    //}
-
-    FileProcessing file("InputFile.txt","OutputFile.txt");
-    file.SetSizeRAM(4 * 1024);
-}
-
