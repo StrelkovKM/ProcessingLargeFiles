5bb4dd4d0e9016c996bcb213bc18801151073dab
turueee <turushkine@inbox.ru>
2025-12-13 17:43:41 +0300

    Update FileProcessing.h

--- FileProcessing/FileProcessing.h
+++ FileProcessing/FileProcessing.h
@@ -4,7 +4,6 @@
 
 #include <vector>
 #include <string>
-#include <algorithm>
 #include <random>
 
 #ifdef _WIN32
@@ -23,32 +22,35 @@
 
 class FileProcessing {
     private:
-    std::fstream file;
-    std::vector<char> buffer;
-    std::vector<char> slice;
-    std::vector<std::vector<char>> memory;
-    size_t memory_size;
-    size_t ram_size;
-    size_t start_write;
-    size_t start_read;
+    std::fstream file;//Объект файла с данными
+    std::vector<char> buffer;//Хранит начало последней считанной строки, которая может быть считана не до конца
+    std::vector<char> slice;//Хранит сырые данные после считывания без разбиения на строки
+    std::vector<std::vector<char>> memory;//Хранит только целые строки из считанных сырых данных
+    size_t memory_size;//Количество данных из файла, которые хранятся в memory
+    size_t ram_size;//Количество оперативной памяти, доступной для работы программы
+    size_t start_write;//Позиция в файле, с которой записываются перемешанные строки
+    size_t start_read;//Позиция в файле, с которой считываются сырые данные
+    size_t size_of_file;//Количество символов в файле
 
-    size_t size_of_file;
+    void sizeRAM();//Вспомогательный метод, передает количество свободной RAM полю ram_size
+    void lenFile();//Вспомогательный метод, передает количество символов в файле полю size_of_file
+    void mergeSlice();//Вспомогательный метод, объединяющий строку, считанную не полностью из поля buffer, с новыми сырыми данными
+
+    void printRAM();//Метод для отладки
 
     public:
-    FileProcessing(const std::string& filename);
-    ~FileProcessing();
+    FileProcessing(const std::string& filename);//Конструктор от имени файла
+    ~FileProcessing();//Деструктор
 
-    void sizeRAM();
-    void setRAMSize(size_t size);
-    void lenFile();
-    void printRAM();
-    void readFile(); //Egor
-    void spliteMemory(); //Misha
-    void clearSlice();
-    void clearBuffer();
-    void shuffleMemory(); //Egor
-    void mergeSlice();
-    void writeFile(); //Misha
+    //Кирилл Стрелков
+    void executeProcessing();//Метод для запуска работы программы
+    void setRAMSize(size_t size);//Метод для указания количества RAM, доступной программе
 
-    void executeProcessing();
+    //Егор Турушкин
+    void readFile();//Метод для считывания сырых данных в slice
+    void shuffleMemory();//Метод для перемешивания строк поля memory (Реализован алгоритм Фишера-Йетса)
+
+    //Михаил Порфирьев
+    void writeFile(); //Метод, записывающий перемешанные строки в файл
+    void spliteMemory(); //Метод, делящий сырые данные на строки
 };
